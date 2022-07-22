04cb13e100e63ba05468d80c3a0744040cfe1d27
Paulo Maldonado <paulomllanos@gmail.com>
2022-07-21 21:25:59 -0400

    Incorporacion de mongoose + plantillas

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Attractions = require('./models/attractions');
+
+mongoose.connect('mongodb://127.0.0.1:27017/tourApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// .then (() => {
+//     console.log('OPEN CONNECTION!!!')
+// })
+// .catch (() => {
+//     console.log('Error MONGO connection!!!')
+// })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makeattractions', async (req, res) => {
+    const atrac = new Attractions({title: 'Torres del Paine', description: 'Parque nacional más importante de Chile'});
+    await atrac.save();
+    res.send(atrac);
 })
 
 app.listen(3000, () => {
